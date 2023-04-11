ac142ffec47fccb24dc304a0d2ee3818128951c8
Magdalena Holubova <mholubova@proton.me>
2023-04-11 22:36:13 +0200

    syntax consistency issue resolved

--- js/script.js
+++ js/script.js
@@ -1,51 +1,52 @@
 function revealer() {
-    let button = document.getElementById('toggleButton');
-    let content = document.getElementById('toggleContent');
+    var button = document.getElementById('toggleButton'),
+        content = document.getElementById('toggleContent');
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         content.classList.toggle('is-visible');
 
-        let initialText = 'Více informací >';
+        var initialText = 'Více informací >';
 
         if (button.textContent.toLocaleLowerCase().includes(initialText.toLowerCase())) {
             button.textContent = 'Sbalit >';
-            button.style.marginTop = "30px"
+            button.style.marginTop = "30px";
         } else {
             button.textContent = initialText;
-            button.style.marginTop = "0"
-        }
-    })
-
-    
+            button.style.marginTop = "0";
+        };
+    });
 }; revealer()
 
 function dropDown() {
-    let boxes = document.querySelectorAll(".lp-faq--box");
+    var boxes = document.querySelectorAll(".lp-faq__box");
 
-    let openbox = (box) => {
-        let content = box.querySelector(".lp-faq--box__content");
-        box.classList.add("lp-faq--box__active");
+    function openbox(box) {
+        var content = box.querySelector(".lp-faq__box--content");
+
+        box.classList.add("lp-faq__box--active");
         content.style.maxHeight = content.scrollHeight + 10 + "px";
     };
 
-    let closebox = (box) => {
-        let content = box.querySelector(".lp-faq--box__content");
-        box.classList.remove("lp-faq--box__active");
+    function closebox(box) {
+        var content = box.querySelector(".lp-faq__box--content");
+
+        box.classList.remove("lp-faq__box--active");
         content.style.maxHeight = null;
     };
 
-    boxes.forEach((box) => {
-        let handle = box.querySelector(".lp-faq--box__handle");
-        let content = box.querySelector(".lp-faq--box__content");
+    boxes.forEach(function (box) {
+        var handle = box.querySelector(".lp-faq__box--handle"),
+            content = box.querySelector(".lp-faq__box--content");
 
-        handle.onclick = () => {
+        handle.addEventListener('click', function () {
             if (content.style.maxHeight) {
                 closebox(box);
             } else {
-                boxes.forEach((box) => closebox(box));
+                boxes.forEach(function (box) {
+                    closebox(box);
+                });
                 openbox(box);
-            }
-        };
+            };
+        });
     });
-
 }; dropDown()
